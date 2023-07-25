399cb8099f05b5b97bb038d64458681e7284b02a
foscjos <fosch006@gmail.com>
2023-07-25 18:29:17 -0500

    WIP

---
+++ EDH-GPT.API/EDH-GPT.API/Controllers/MTGJsonController.cs
@@ -0,0 +1,21 @@
+﻿namespace EDH_GPT.API.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class MTGJsonController : ControllerBase
+{
+    private readonly ILogger<MTGJsonController> _logger;
+
+    public MTGJsonController(ILogger<MTGJsonController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "Get all")]
+    public string Get()
+    {
+        return "Hello world";
+    }
+}
